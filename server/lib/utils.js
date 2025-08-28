44c6f22296e6fef0f975f1b66d1a37d68376265e
Dhirajk56 <Dhirajkumarxyz13@gmail.com>
2025-08-28 19:17:30 +0530

    changes in utils

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 //function to generate token
 
 export const generateToken = (userid) => {
-  const token = jwt.sign({ userid }, process.env.JWT_SECRET);
+  const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
   return token;
 };
